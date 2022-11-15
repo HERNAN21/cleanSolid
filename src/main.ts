45c1d1e12a59b56d4cb678ccb72e5cc2d7be0d99
HERNAN21 <hernanrojasutani@gmail.com>
2022-11-15 12:30:37 -0500

    OCP y LSP

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import './style.css'
+// import './solid/02-open-close-a';
+import './solid/03-liskov-a'
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+app.innerHTML = `
+  <h1>CleanCode y SOLID</h1>
+  <span>Revisar la consola de JavaScript</span>
+`
+
---
+++ src/solid/03-liskov-b.ts
@@ -0,0 +1,75 @@
+export abstract class Vehiculo{
+    // getNumberOfSeats(): number{
+    //     throw Error("not implemented")
+    // };
+
+    abstract getNumberOfSeats(): number;
+
+}
+
+export class Tesla extends Vehiculo {
+
+    constructor( private numberOfSeats: number ) {
+        super();
+    }
+
+    getNumberOfSeats() {
+        return this.numberOfSeats;
+    }
+    
+}
+
+export class Audi extends Vehiculo{
+
+    constructor( private numberOfSeats: number ) {
+        super();
+    }
+
+    getNumberOfSeats() {
+        return this.numberOfSeats;
+    }
+}
+
+export class Toyota extends Vehiculo{
+
+    constructor( private numberOfSeats: number ) {
+        super();
+    }
+
+    getNumberOfSeats() {
+        return this.numberOfSeats;
+    }
+}
+
+export class Honda extends Vehiculo{
+
+    constructor( private numberOfSeats: number ) {
+        super();
+    }
+
+    getNumberOfSeats() {
+        return this.numberOfSeats;
+    }
+}
+
+export class Volvo extends Vehiculo{
+
+    constructor( private numberOfSeats: number ) {
+        super();
+    }
+
+    getNumberOfSeats() {
+        return this.numberOfSeats;
+    }
+}
+
+export class Ford extends Vehiculo{
+
+    constructor( private numberOfSeats: number ) {
+        super();
+    }
+
+    getNumberOfSeats() {
+        return this.numberOfSeats;
+    }
+}
